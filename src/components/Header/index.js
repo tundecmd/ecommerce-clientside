b52d0d7163e1e1f1366f48c22e2baa2a40b873a9
Adepoju Lateef <ademustexcel@gmail.com>
2021-04-26 15:55:00 +0100

    initial commit, added categories dropdown

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./styles.css"
+/**
+* @author
+* @function Header
+**/
+
+const Header = (props) => {
+  return(
+    <div className="header">
+        
+    </div>
+   )
+
+ }
+
+export default Header
