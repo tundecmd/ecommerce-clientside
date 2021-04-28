febd203f06bb5d7976caf7b2465b5c0f99fa62c7
Adepoju Lateef <ademustexcel@gmail.com>
2021-04-28 18:19:07 +0100

    added fetch product api and render product list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import HomePage from './containers/HomePage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ProductListPage from './containers/ProductListPage';
+
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage}  />
+          <Route path="/:slug" component={ProductListPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import categoryReducer from "./category.reducer";
+import productReducer from "./product.reducer";
 
 const rootReducer = combineReducers({
-    category: categoryReducer
+    category: categoryReducer,
+    product: productReducer
 })
 
 export default rootReducer;
